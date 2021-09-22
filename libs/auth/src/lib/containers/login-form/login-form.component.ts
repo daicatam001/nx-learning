24da9dc82d33b55dbac39b2a3a68cc580125ed5a
tampt <tampt@techasians.com>
2021-09-22 16:05:01 +0700

    add server

--- libs/auth/src/lib/containers/login-form/login-form.component.ts
+++ libs/auth/src/lib/containers/login-form/login-form.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
+import {Authenticate} from '@nx-learning/data-models';
 
 @Component({
   selector: 'nx-learning-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
+  @Output() submit = new EventEmitter<Authenticate>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login(authenticate: Authenticate) {
+    this.submit.emit(authenticate);
   }
 
 }
---
+++ libs/data-models/src/lib/data-models.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+export { Authenticate } from './authenticate';
+@NgModule({
+  imports: [CommonModule],
+})
+export class DataModelsModule {}
